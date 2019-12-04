1945b0458ecafd666d4ada2dba8e4095d7c04ceb
Konstantin Yakushin <y231966mdk@gmail.com>
2019-12-04 19:44:58 +0300

    refactoring: No need to store material

--- src/hittables/box_shape.rs
+++ src/hittables/box_shape.rs
@@ -2,76 +2,74 @@ use crate::hittables::{FlipNormal, HittableList, XYRect, XZRect, YZRect};
 use crate::math::Vec3;
 use crate::physics::{HitRecord, Hitable, Material, Ray, AABB};
 
-pub struct BoxShape<T: Material> {
+pub struct BoxShape {
     pmin: Vec3,
     pmax: Vec3,
-    material: T,
     hit_objects: HittableList,
 }
 
-impl<T: Material + Clone + 'static> BoxShape<T> {
-    pub fn new(p0: Vec3, p1: Vec3, material: T) -> Self {
+impl BoxShape {
+    pub fn new<T: Material + Clone + 'static>(p_min: Vec3, p_max: Vec3, material: T) -> Self {
         let mut objects = HittableList::new(Vec::with_capacity(6));
         objects.push(XYRect::new(
-            p0.x(),
-            p1.x(),
-            p0.y(),
-            p1.y(),
-            p1.z(),
+            p_min.x(),
+            p_max.x(),
+            p_min.y(),
+            p_max.y(),
+            p_max.z(),
             material.clone(),
         ));
         objects.push(FlipNormal::new(XYRect::new(
-            p0.x(),
-            p1.x(),
-            p0.y(),
-            p1.y(),
-            p0.z(),
+            p_min.x(),
+            p_max.x(),
+            p_min.y(),
+            p_max.y(),
+            p_min.z(),
             material.clone(),
         )));
         objects.push(XZRect::new(
-            p0.x(),
-            p1.x(),
-            p0.z(),
-            p1.z(),
-            p1.y(),
+            p_min.x(),
+            p_max.x(),
+            p_min.z(),
+            p_max.z(),
+            p_max.y(),
             material.clone(),
         ));
         objects.push(FlipNormal::new(XZRect::new(
-            p0.x(),
-            p1.x(),
-            p0.z(),
-            p1.z(),
-            p0.y(),
+            p_min.x(),
+            p_max.x(),
+            p_min.z(),
+            p_max.z(),
+            p_min.y(),
             material.clone(),
         )));
         objects.push(YZRect::new(
-            p0.y(),
-            p1.y(),
-            p0.z(),
-            p1.z(),
-            p1.x(),
+            p_min.y(),
+            p_max.y(),
+            p_min.z(),
+            p_max.z(),
+            p_max.x(),
             material.clone(),
         ));
 
         objects.push(FlipNormal::new(YZRect::new(
-            p0.y(),
-            p1.y(),
-            p0.z(),
-            p1.z(),
-            p0.x(),
+            p_min.y(),
+            p_max.y(),
+            p_min.z(),
+            p_max.z(),
+            p_min.x(),
             material.clone(),
         )));
 
         BoxShape {
-            pmin: p0,
-            pmax: p1,
-            material,
+            pmin: p_min,
+            pmax: p_max,
             hit_objects: objects,
         }
     }
 }
 
-impl<T: Material> Hitable for BoxShape<T> {
+impl Hitable for BoxShape {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
         self.hit_objects.hit(r, t_min, t_max)
     }
